d9f1f76f96528a8743b084d2e10f432b07c1540d
hyokangs <34936244+hyokangs@users.noreply.github.com>
2020-07-23 17:08:06 -0700

    Update mod_outer_links.js

--- assets/js/mod_outer_links.js
+++ assets/js/mod_outer_links.js
@@ -2,7 +2,7 @@ $(function () {
     // add `target="_blank"` to outgoing links.
     var host = document.location.host;
     $("a[href]").each(function() {
-        var re = new RegExp(host, "abcd"); /* "g" */
+        var re = new RegExp(host, "g");
         if ($(this).attr("href").match(/\/\//) && !$(this).attr("href").match(re)) {
             $(this).attr("target", "_blank");
         }
